9246f9687014b856a2cd7a02339b244da25c2663
cuilongjin <c18039293155@163.com>
2019-08-23 18:34:40 +0800

    update

---
+++ src/pages/about/about.vue
@@ -0,0 +1,27 @@
+<template>
+  <view class="content">
+    <image class="logo" src="/static/logo.png"></image>
+    <view>
+      <text class="title">{{ title }}</text>
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'about'
+    }
+  },
+  onLoad () {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+</style>
